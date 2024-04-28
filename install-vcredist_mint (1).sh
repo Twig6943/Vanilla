afca814b292ed6bd2f69e96abd8576ded8761bce
Twig6943 <119701717+Twig6943@users.noreply.github.com>
2024-04-28 12:35:04 +0300

    Add files via upload

---
+++ install-vcredist_mint (1).sh
@@ -0,0 +1,20 @@
+# Latest Microsoft Visual C++ Redistributable Version
+wine vcredist/VC_redist.x86_latest.exe
+wine vcredist/VC_redist.x64_latest.exe
+# Visual Studio 2013 (VC++ 12.0)
+wine vcredist/vcredist_x86_2013.exe
+wine vcredist/vcredist_x64_2013.exe
+# Visual Studio 2012 (VC++ 11.0)
+wine vcredist/vcredist_x86_2012.exe
+wine vcredist/vcredist_x64_2012.exe
+# Visual Studio 2010
+wine vcredist/vcredist_x86.exe
+wine vcredist/vcredist_x64.exe
+# Visual Studio 2008
+wine vcredist/vcredist_x86_2008.exe
+wine vcredist/vcredist_x64_2008.exe
+# Visual Studio 2005
+wine vcredist/vcredist_x86_2005.exe
+wine vcredist/vcredist_x64_2005.exe
+
+echo "\n\ndone"
